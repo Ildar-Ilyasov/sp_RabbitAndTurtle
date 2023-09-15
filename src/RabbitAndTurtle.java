94f8481f25c62c8976ab1fe48b8480dc813d8d91
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-09-15 13:31:25 +0300

    Initial commit

---
+++ src/RabbitAndTurtle.java
@@ -0,0 +1,9 @@
+public class RabbitAndTurtle extends Thread {
+    public static void main(String[] args) {
+        AnimalThread rabbit = new AnimalThread("Кролик", 5);
+        AnimalThread turtle = new AnimalThread("Черепаха", 10);
+        rabbit.start();
+        turtle.start();
+    }
+
+}
